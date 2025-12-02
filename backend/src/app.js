4e0a8621e9985dc1fed18152b6fc13d839c00d38
Serhat GENÇ <gncsrht05@gmail.com>
2025-12-02 16:23:58 +0300

    istek url ler düzeltilidi

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://localhost:5000", 
- "https://epharmacy-dashboard-jet.vercel.app/login",
+ "https://epharmacy-dashboard-jet.vercel.app",
 ];
 
 
