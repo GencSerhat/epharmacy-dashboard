b13e9343f4b8d9a432864fa5876cc9cd0a65389b
Serhat GENÇ <gncsrht05@gmail.com>
2025-12-02 15:40:41 +0300

    backend cors ayarı yapıldı

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+
 import userRoutes from "./routes/userRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 import customersRoutes from "./routes/customersRoutes.js";
@@ -8,25 +9,44 @@ import ordersRouter from "./routes/ordersRoutes.js";
 import productsRouter from "./routes/productsRoutes.js";
 import suppliersRouter from "./routes/suppliersRoutes.js";
 
-
 const app = express();
 
-//MiddlewARES
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "http://localhost:5000", 
+  "https://SENIN-UYGULAMAN.vercel.app", // 🔴 BURAYI kendi Vercel domaininle değiştir
+];
+
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
+ 
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
 app.use(morgan("dev"));
 app.use(express.json());
 
-
-
-
 app.use("/api/user", userRoutes);
-app.use("/api",dashboardRoutes);
+app.use("/api", dashboardRoutes);
 app.use("/api/customers", customersRoutes);
 app.use("/api/orders", ordersRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/suppliers", suppliersRouter);
-
-
 
 export default app;
