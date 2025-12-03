541f30fb56009bb9b58025e5c2222a143e0321bc
Serhat GENÇ <gncsrht05@gmail.com>
2025-12-03 20:27:29 +0300

    comment düzeniltildi

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //backend bağlantı
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-console.log("🔗 API BASE_URL:", BASE_URL);   // ⬅⬅ BUNU EKLE
+console.log("🔗 API BASE_URL:", BASE_URL);   // kontrol
 const api = axios.create({
   baseURL: BASE_URL,
 });
